236b7787016dae64f5801288c156c2841d5357e0
Jonathan Chiu <jonathanchiu7@gmail.com>
2014-11-11 00:46:10 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+    http = require('http'),
+    app = express(),
+    httpServer = http.createServer(app);
+
+
+app.set('port', 3000);
+app.use(express.static(__dirname + '/public'));
+
+
+httpServer.listen(app.get('port'), function () {
+    console.log("Express server listening on port %s.", httpServer.address().port);
+});
