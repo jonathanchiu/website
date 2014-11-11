843b54e4e4b7448cdd768b9d700d897250e83150
Jonathan Chiu <jonathanchiu7@gmail.com>
2014-11-11 00:54:05 -0500

    Changed port number for server

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ var express = require('express'),
     app = express(),
     httpServer = http.createServer(app);
 
-
-app.set('port', 3000);
+app.set('port', 80);
 app.use(express.static(__dirname + '/public'));
 
-
 httpServer.listen(app.get('port'), function () {
-    console.log("Express server listening on port %s.", httpServer.address().port);
+  console.log("Express server listening on port %s.", httpServer.address().port);
 });
